0f087e793dcce72b91ee08b971ec5849c3be7ebd
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-11-07 21:07:31 +0200

    save images into state compo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Header from './components/Header';
-import Search from './components/Search'
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-
+// git remote add origin https://github.com/Mohamadmahgoub910/imageGalleryReactApp1.git
+// git push -u origin main
 const App = () => {
-  // Make useState synced with search input 
+  // Make useState synced with search input
   const [word, setWord] = useState('');
-  // Make Handle Submit 
+  // set images for display
+  const [images, setImages] = useState([]);
+  // Make Handle Submit
   const HandleSearchSubmit = (e) => {
     e.preventDefault();
-    // console.log(word)
+    console.log(word)
+    console.log(images);
     // console.log(process.env.REACT_APP_UNSPLASH_KEY)
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images])
+        // console.log(images);
       })
       .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+    setWord("");
+  };
   return (
     <div>
       <Header title="Images Gallery" />
