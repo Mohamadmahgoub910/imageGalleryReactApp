1409a95b92ddb5ff86818c4d98b9b26d1398178e
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-11-08 01:56:33 +0200

    update the delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
 
+import { useState } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import ImageCard from "./components/ImageCard";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 // git remote add origin https://github.com/Mohamadmahgoub910/imageGalleryReactApp1.git
@@ -15,15 +17,15 @@ const App = () => {
   // Make Handle Submit
   const HandleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word)
-    console.log(images);
+    // console.log(word)
+    // console.log(images);
     // console.log(process.env.REACT_APP_UNSPLASH_KEY)
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images])
+        setImages([{ ...data, title: word }, ...images])
         // console.log(images);
       })
       .catch((err) => {
@@ -31,10 +33,27 @@ const App = () => {
       });
     setWord("");
   };
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
   return (
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={HandleSearchSubmit} />
+      {/* {!!images.length && <ImageCard image={images[0]} />} */}
+      {/* {images.map((image, i) => (
+        <ImageCard key={i} image={image} />
+      ))} */}
+      {/* Make some design to display true */}
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i} className="pd-3">
+              <ImageCard image={image} deleteImage={handleDeleteImage} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 };
