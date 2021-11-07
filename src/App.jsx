c59c69f61c5c309bbd9a2034654cede8b2c7276c
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-11-07 09:44:15 +0200

    set state for search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Header from './components/Header';
+import Search from './components/Search'
+
 
 const App = () => {
+  // Make useState synced with search input 
+  const [word, setWord] = useState('');
+  // Make Handle Submit 
+  const HandleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word)
+  }
   return (
     <div>
       <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={HandleSearchSubmit} />
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from "react-bootstrap";
+
 
 const Header = (props) => {
     const { title } = props;
-    return (<>
-        <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/">{title} </Navbar.Brand>
+    const navBarStyle = {
+        backgroundColor: 'deepskyblue'
+    };
+    return (
+        <Navbar style={navBarStyle} variant="dark">
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
         </Navbar>
-    </>)
+    )
 }
 export default Header
