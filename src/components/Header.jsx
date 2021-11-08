74d2f29ca7e6e9dfadbb7d08ad69ef391b32f6f8
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2021-11-08 02:52:36 +0200

    added text logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
 
+const navbarStyle = {
+    backgroundColor: '#eeeeee',
+};
 
-const Header = (props) => {
-    const { title } = props;
-    const navBarStyle = {
-        backgroundColor: 'deepskyblue'
-    };
+const Header = ({ title }) => {
     return (
-        <Navbar style={navBarStyle} variant="dark">
+        <Navbar style={navbarStyle} variant="light">
             <Container>
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
+                <Logo alt={title} style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
             </Container>
         </Navbar>
-    )
-}
-export default Header
+    );
+};
+
+export default Header;
